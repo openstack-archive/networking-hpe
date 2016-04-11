868db8bab98bf6a0794d065a8e15763e12728352
ashish-kumar-gupta <ashish-kumar.gupta@hpe.com>
2016-04-11 00:14:27 -0700

    Nit Copyright
    Make sure the copyright is with the latest year of code development
    
    Change-Id: I4a7af6467bd6e210fcb2e2c7976523eb230da21f

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+# Copyright (c) 2016 Hewlett-Packard Enterprise Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
